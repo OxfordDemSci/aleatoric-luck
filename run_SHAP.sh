8a98487c260e1d21d4e2d46b8a6fac0ee7f3e64f
Ava Keeling <tej036@cluster3.hpc.in.bmrc.ox.ac.uk>
2026-05-26 13:13:58 +0100

    added batch script

---
+++ run_SHAP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J aleatoric
+#SBATCH -o logs/%x-%j.out
+#SBATCH -e logs/%x-%j.err
+#SBATCH -t 02:00:00
+#SBATCH -p short
+
+module purge
+module load Python/3.11
+
+cd /gpfs3/users/mills/tej036/aleatoric-luck
+
+source ~/venvs/aleatoric-luck/bin/activate
+
+python src/SHAP_vals.py
+
+echo "All done!"
